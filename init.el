ff57f95de6a5c4b511b9f220aaaefe3989eb11e6
Jakub Arnold <darthdeus@gmail.com>
2014-05-15 12:45:29 +0200

    Remove le-get

--- init.el
+++ init.el
@@ -32,15 +32,6 @@
 
 ;(global-auto-complete-mode 1)
 
-(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
 ;; Automatically install a pre-defined list of packages
 (dolist (p my-packages)
   (when (not (package-installed-p p))
