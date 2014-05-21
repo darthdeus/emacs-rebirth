388f6a6c8198645ebfe4776f4a4683f480836f79
Jakub Arnold <darthdeus@gmail.com>
2014-05-21 21:44:48 +0200

    Add customized variables

--- init.el
+++ init.el
@@ -166,6 +166,15 @@
 ; emacs as word processor - read the whole thread https://lists.gnu.org/archive/html/emacs-devel/2013-11/msg00594.html
 
 ; My old config https://gist.github.com/darthdeus/d645f9e87db4cb8a9721
-
-
-; http://code.org/teach
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+)
