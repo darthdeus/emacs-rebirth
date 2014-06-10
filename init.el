da6f68e177c573dc898b70ad9986d37322c89eab
Jakub Arnold <darthdeus@gmail.com>
2014-06-10 11:02:16 +0200

    Get rid of SHM

--- init.el
+++ init.el
@@ -93,7 +93,6 @@
 ; TODO - make this work
 ; (define-key key-translation-map (kbd ",f") (kbd "C-c p f"))
 
-;(add-hook 'haskell-mode-hook 'structured-haskell-mode)
 ;; TODO - check auto-fill mode
 
 ; check why this doesn't work
