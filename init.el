c84b6e42a1ee9e3b2170fdade1b0760a5cd42be1
Jakub Arnold <darthdeus@gmail.com>
2014-06-10 11:02:58 +0200

    Setup interactive haskell mode instead of inferior mode

--- init.el
+++ init.el
@@ -108,6 +108,39 @@
             (global-set-key (kbd "s-s") 'ghc-save-buffer)
 	    ))
 
+(eval-after-load "haskell-mode"
+  '(progn
+     (define-key haskell-mode-map (kbd "C-,") 'haskell-move-nested-left)
+     (define-key haskell-mode-map (kbd "C-.") 'haskell-move-nested-right)))
+
+(global-set-key (kbd "C-x a r") 'align-regexp)
+
+(define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-process-log t))
+
+(define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+;; (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-jump-to-def)
+
+;; (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+;; (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+;; (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+;; (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+(custom-set-variables
+  '(haskell-process-type 'cabal-repl))
+
+
 (setq ghc-display-error 'minibuffer)
 
 (defun ac-haskell-mode-setup ()
