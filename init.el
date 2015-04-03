63950865ae6828431c7cf1a140ea10ce88829177
Jakub Arnold <darthdeus@gmail.com>
2015-04-04 00:47:57 +0200

    Enable paredit everywhere

--- init.el
+++ init.el
@@ -85,6 +85,17 @@
 (global-undo-tree-mode 0)
 (global-auto-complete-mode 1)
 
+(setq inferior-lisp-program "/usr/local/bin/clisp")
+
+(add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+(add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+(add-hook 'racket-mode-hook           #'enable-paredit-mode)
+(add-hook 'clojure-mode-hook          #'enable-paredit-mode)
+
 ;; Coq
 (load-file "/usr/local/share/emacs/site-lisp/ProofGeneral/generic/proof-site.el")
 (setq auto-mode-alist (cons '("\\.v$" . coq-mode) auto-mode-alist))
