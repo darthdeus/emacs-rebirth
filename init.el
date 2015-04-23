acc271a6e4e128bbd54560bf0a5fc8d92e068424
Jakub Arnold <darthdeus@gmail.com>
2015-04-23 10:39:52 +0200

    Change initial frame position

--- init.el
+++ init.el
@@ -16,7 +16,8 @@
 (when (not (window-system)) (menu-bar-mode 0))
 
 ;; Default window position
-(setq initial-frame-alist '((top . 90) (left . 180) (width . 170) (height . 60)))
+(setq initial-frame-alist '((top . 0) (left . 0) (width . 140) (height . 45)))
+;; (setq initial-frame-alist '((top . 90) (left . 180) (width . 170) (height . 60)))
 
 (setq backup-directory-alist '(("" . "~/.emacs.d/emacs-backup")))
 
